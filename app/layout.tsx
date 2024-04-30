951ac94e1a592e875d28ed8ce7bedc6b44cf3c0a
eduardneague <eduard.neague1337@gmail.com>
2024-04-30 14:20:10 +0300

    font again

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,12 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const montserrat = Montserrat({ subsets: ["latin"]});
+const montserrat = Montserrat({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display:'swap',
+  fallback: ['Arial', 'sans-serif'],
+});
 
 export const metadata: Metadata = {
   title: "adfilm",
